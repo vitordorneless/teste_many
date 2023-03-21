5c286eb0ea81d70648df3e6c2b29dc2c972920a3
vitordorneless <vitordorneles@hotmail.com>
2023-03-21 11:01:39 -0300

    testando rotas

--- application/views/home.php
+++ application/views/home.php
@@ -1,7 +1,2 @@
 <?php
-echo 'oi';
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
+echo 'oi<br>';
